170b5a1ba33ae711b3c72b8e1731acec4244c134
BARATH R <barath.240013@ece.ritchennai.edu.in>
2026-02-26 15:57:33 +0530

    0 ms (100%) 8.7 MB (83.16%) .cpp 02-26-2026

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one-02-26-2026-15-57-35.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        int n = s.size();
+        int steps = 0;
+        int carry = 0;
+        
+        for (int i = n - 1; i > 0; --i) {
+            int bit = (s[i] - '0') + carry;
+            if (bit % 2 == 0) {
+               
+                steps += 1;
+            } else {
+           
+                steps += 2;
+                carry = 1;
+            }
+        }
+        
+        // If there's a carry left at the most significant bit
+        // we need one more division step
+        if (carry == 1) {
+            steps += 1;
+        }
+        
+        return steps;
+    }
+};
