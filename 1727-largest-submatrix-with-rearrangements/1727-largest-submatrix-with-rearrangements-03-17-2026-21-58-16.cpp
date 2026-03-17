d485fd4b19d44340b5ba15078c06161fac202339
BARATH R <barath.240013@ece.ritchennai.edu.in>
2026-03-17 21:58:09 +0530

    44 ms (35.11%) 89.2 MB (61.7%) .cpp 03-17-2026

---
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements-03-17-2026-21-58-16.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        for (int i = 1; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (matrix[i][j] == 1)
+                    matrix[i][j] += matrix[i - 1][j];
+
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            vector<int> row = matrix[i];
+            sort(row.begin(), row.end(), greater<int>());
+            for (int j = 0; j < n; ++j)
+                if (row[j] > 0)
+                    ans = max(ans, row[j] * (j + 1));
+        }
+        return ans;
+    }
+};
